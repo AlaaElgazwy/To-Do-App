bd148e08e7f86239aa59bb8a96696513f7d1b824
alaa <alaa@localhost.localdomain>
2025-08-27 19:38:53 +0300

    second commit

--- mytodo/todo/urls.py
+++ mytodo/todo/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('update/<str:pk>/',views.updateTask,name='update_task'),
+    path('delete/<str:pk>/',views.deleteTask,name='delete_task'),
+
 ]
--- mytodo/todo/views.py
+++ mytodo/todo/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Task
+from .forms import TaskForm
 # Create your views here.
 def index(request):
     tasks=Task.objects.all()
+    form=TaskForm()
+    if request.method == 'POST':
+        form=TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+        
+    return render(request,'index.html',{'tasks':tasks, 'form':form})
+def updateTask(request ,pk):
+    task=Task.objects.get(id=pk)
+    form=TaskForm(instance=task)
 
-    return render(request,'index.html',{'tasks':tasks})
+    if request.method == 'POST':
+        form =TaskForm(request.POST,instance=task)
+        if form.is_valid():
+            form.save()
+        return redirect('/')    
+    return render(request,'update.html',{'form':form})
+def deleteTask(request,pk):
+    task=Task.objects.get(id=pk)
+    if request.method=='POST':
+       task.delete()
+
+       return redirect('/')
+    return render(request,'delete.html',{'task':task})
